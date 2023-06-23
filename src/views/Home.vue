27d4beba3a98c0d1a094dcdb0c6da23c0f7ff715
Marcelo Rocha <marcerocha@live.com>
2023-06-22 23:48:09 -0300

    base para mi idea

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<h1>home </h1>
+<p> {{ userStore.userData }}</p>
+    </div>
+</template>
+
+<script setup>
+
+import {useUserStore} from '../stores/user'
+
+const userStore=useUserStore()
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+<h1>register</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
